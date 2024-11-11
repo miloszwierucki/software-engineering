4d56a8aeb07c86a65fa5ac2a28d50e2cbe9bbb5c
miloszwierucki <milosz@wierucki.com>
2024-11-11 20:21:51 +0100

    Setup project for IO

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+// vite.config.ts
+import path from "path";
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    TanStackRouterVite(),
+    viteReact(),
+    // ...,
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
